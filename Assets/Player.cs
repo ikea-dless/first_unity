5f46aefbabbb04a945378daf9a213cfe004b8e29
Takahiro Ikeda <ikea_dless@icloud.com>
2014-12-14 15:56:21 +0900

    first commit

---
+++ Assets/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 6;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody.MovePosition (transform.position + transform.forward * Time.deltaTime * speed);
+
+		if (Input.GetButtonDown ("Fire1")) {
+			GetComponent<Animator>().SetTrigger("JUMP");
+		}
+		if (Input.GetButtonDown ("Fire2")) {
+			GetComponent<Animator>().SetTrigger("SLIDE");
+		}
+	}
+
+	void OnTriggerEnter (Collider colider) {
+		var stateInfo = GetComponent<Animator>().GetCurrentAnimatorStateInfo (0);
+		bool isRun = stateInfo.IsName("Base Layer.RUN00_F");
+
+		bool isHigh = colider.CompareTag ("High");
+		bool isLow = colider.CompareTag ("Low");
+
+		bool isJump = stateInfo.IsName("Base Layer.JUMP00");
+		bool isSlide = stateInfo.IsName ("Base Layer.SLIDE00");
+
+		if( (isRun == true ) ||
+			(isJump == true && isHigh == true) ||
+			(isSlide == true && isLow == true)) {
+			GetComponent<Animator>().SetTrigger ("DEAD");
+			speed = 0;
+		}
+	}
+}
